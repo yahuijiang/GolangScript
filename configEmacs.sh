366879c879ede37c9a012b4a06a870e263df3d02
jasson <jasson@localhost.localdomain>
2017-07-02 17:56:24 +0800

    add config emcas script

--- configEmacs.sh
+++ configEmacs.sh
@@ -2,17 +2,26 @@
 
 # please install emacs >24 firstly
 
+
 #test the status of emacs
-whereis emacs -b > /dev/null
+emacs --help 2> /dev/null
 
 if [ $? -eq 0 ]
 then 
    echo "emacs installed, continue..."
 else 
    echo "emacs has not insalled, exit!"
+   exit 0
 fi
 
+emacsconfigfile="~/.emacs.d"
+if [ -f "$emacsconfigfile" ]
+then
+  echo "backup the original emacs config"
+  mv ~/.emacs.d ~/.emacs.d.bak
+fi
 #config emacs, install some package
 
-git clone git@github.com:yahuijiang/emacsconfig.git
+#git clone git@github.com:yahuijiang/emacsconfig.git ~/.emacs.d
 
+sudo cp ./goext/* $GOBIN
--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-#Download package
-wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+#check the status of golang
 
-#unzip the package
+go env 2> /dev/null
 
-tar -zxvf go1.8.3.linux-amd64.tar.gz -C /usr/local/go
-
-#config the golang env
-
-echo 'export GOROOT=/usr/local/go' >> ~/.bashrc 
-echo 'export GOBIN=$GOROOT/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
-echo 'export GOPATH=~/workspace/src/golang/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$GOPATH' >> ~/.bashrc
+if [ $? -eq 0 ]
+then
+  echo "golang installed, this step will skip"
+else
+  echo "golang has not install, start install golang, please waite"
+  #Download package
+  #wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+  #unzip the package
+  tar -zxvf go1.8.3.linux-amd64.tar.gz -C /usr/local/
+  #config the golang env
+  echo 'export GOROOT=/usr/local/go' >> ~/.bashrc 
+  echo 'export GOBIN=$GOROOT/bin' >> ~/.bashrc
+  echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
+  echo 'export GOPATH=~/workspace/golang/' >> ~/.bashrc
+  echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+  source ~/.bashrc
+  go env 2> /dev/null
+  if [ $? -eq 0 ]
+  then 
+    echo "install successfully. "
+  else 
+    echo "install failed"
+  fi
+fi
 
